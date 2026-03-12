65b2b06473336d0051ce9a5ea6a8f9d7c4a3c75d
amrxtgh <ghimireamrit619@gmail.com>
2026-03-12 17:39:31 +0545

    main.c for testing

---
+++ virtualization/cpu/process-basics/main.c
@@ -0,0 +1,23 @@
+#include "proc.h"
+#include <stdio.h>
+
+int main(void) {
+
+  printf("test\n");
+
+  struct proc *p1 = allocproc();
+
+  if (!p1) {
+    printf("allocproc failed\n");
+    return 1;
+  }
+
+  printf("Process 1: pid=%d, state=%d\n", p1->pid, p1->state);
+
+  struct proc *child = vfork(p1);
+  if (!child) {
+    printf("vfork failed\n");
+    return 1;
+  }
+  printf("Child process: pid=%d, state=%d\n", child->pid, child->state);  
+}
