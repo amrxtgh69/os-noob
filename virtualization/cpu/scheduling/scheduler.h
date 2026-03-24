c2b743963c39c202871e1b3f1b74bfdbc218f96e
amrxtgh <ghimireamrit619@gmail.com>
2026-03-24 17:53:06 +0545

    simple fix

--- virtualization/cpu/scheduling/scheduler.h
+++ virtualization/cpu/scheduling/scheduler.h
@@ -7,7 +7,7 @@ void scheduler_init();
 /* add a process to scheduler when it becomes READY */ 
 void scheduler_add_process(Process* p);
 /* Update scheduler state after the process ran for tick_elapsed */ 
-Process* scheduler_process_ticked(Process* p, int tick_elapsed);
+Process* scheduler_process_ticked(Process* p, int tick_elapsed, int global_time);
 /* cleanup or the keeping of the state when the process finished */ 
 void scheduler_finished_process(Process* p);
 
