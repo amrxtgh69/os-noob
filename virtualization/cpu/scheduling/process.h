aa5d44da8da94eee54ed17216bfc4df77c4166e6
amrxtgh <ghimireamrit619@gmail.com>
2026-03-29 01:43:12 +0545

    add the definition doc

--- virtualization/cpu/scheduling/process.h
+++ virtualization/cpu/scheduling/process.h
@@ -6,12 +6,13 @@ typedef enum { CREATED, READY, RUNNING, BLOCKED, TERMINATED } State;
 
 // the attribute of the process
 typedef struct Process {
-    int pid;
+    int pid;              // The unique id of the process
     int arrival_time;
-    int burst_time;
-    int remaining_time;
-    int start_time;
-    int completion_time;
+    int burst_time;       // The total amount of CPU time a process need to complete its execution.
+    int remaining_time;   // How much CPU time still needed for a process to complete its execution
+                          // This decreases as the process executed: at the start remaining_time = burst_time.
+    int start_time;       // It is the exact time(tick) when a process first gets CPU time. 
+    int completion_time;  // Tick when process finishes execution
     State state;
 } Process;
 
