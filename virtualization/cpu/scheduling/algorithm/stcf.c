54a6a37b66c7e8980ac144f0116ffa2676b535ce
amrxtgh <ghimireamrit619@gmail.com>
2026-03-30 22:56:46 +0545

    init

---
+++ virtualization/cpu/scheduling/algorithm/stcf.c
@@ -0,0 +1,12 @@
+/*
+ * 
+ * */
+#include "../process.h"
+
+void stcf_init() {}
+
+void stcf_add_process(Process* p) {}
+
+Process* stcf_tick(Process* p, int tick, int global_time) {}
+
+void stcf_finished() {}
