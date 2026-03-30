0c14d326448328bd22621b0f140bc094e3ae9171
amrxtgh <ghimireamrit619@gmail.com>
2026-03-30 22:56:32 +0545

    update: process.h

--- virtualization/cpu/scheduling/process.h
+++ virtualization/cpu/scheduling/process.h
@@ -9,8 +9,10 @@ typedef struct Process {
     int pid;              // The unique id of the process
     int arrival_time;
     int burst_time;       // The total amount of CPU time a process need to complete its execution.
+
     int remaining_time;   // How much CPU time still needed for a process to complete its execution
                           // This decreases as the process executed: at the start remaining_time = burst_time.
+
     int start_time;       // It is the exact time(tick) when a process first gets CPU time. 
     int completion_time;  // Tick when process finishes execution
     State state;
