76a0ab78fa2f490315ccba93e813ffc75af22b49
amrxtgh <ghimireamrit619@gmail.com>
2026-04-02 01:55:07 +0545

    add: enqueue and dequeue logic

--- virtualization/cpu/scheduling/algorithm/rr.c
+++ virtualization/cpu/scheduling/algorithm/rr.c
@@ -1,9 +1,29 @@
 #include "../process.h"
+#include <stddef.h>
 
 #define MAX_PROCESS 10
 
 Process* queue[MAX_PROCESS];
 int count = 0;
+int rear = 0;
+int front = 0;
+
+void enqueue(Process* p) {
+  if (count == MAX_PROCESS) { return; }
+  queue[rear] = p;
+  rear = (rear + 1) % MAX_PROCESS;
+  count++;
+}
+
+Process* dequeue() {
+  if (count == 0) {
+    return NULL;
+  }
+  Process* p = queue[front];
+  front = (front + 1) % MAX_PROCESS;
+  count--;
+  return p;
+}
 
 void rr_init() {}
 
