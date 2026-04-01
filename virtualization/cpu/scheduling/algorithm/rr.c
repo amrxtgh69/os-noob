f75e5307a1182c52f891c69b92fac8d3ca4392e2
amrxtgh <ghimireamrit619@gmail.com>
2026-04-02 02:16:29 +0545

    add: rr algorithm simple prototype only

--- virtualization/cpu/scheduling/algorithm/rr.c
+++ virtualization/cpu/scheduling/algorithm/rr.c
@@ -2,6 +2,9 @@
 #include <stddef.h>
 
 #define MAX_PROCESS 10
+#define QUANTUM 2
+
+static int time_used = 0;
 
 Process* queue[MAX_PROCESS];
 int count = 0;
@@ -33,13 +36,30 @@ void rr_init() {
 
 void rr_add_process(Process* p) {
   enqueue(p);
-  count++;
 }
 
 Process* rr_tick(Process* p, int tick, int global_time) {
+  if (p == NULL) {
+    p = dequeue();
+    time_used = 0;
+  }
 
+  if (p == NULL) return NULL;
+  p->remaining_time--;
+  time_used++;
+
+  if (p->remaining_time == 0) {
+    time_used = 0;
+    return NULL;
+  }
+  if (time_used >= QUANTUM) {
+    enqueue(p);
+    time_used = 0;
+    return NULL;
+  }
+  return p;
 }
 
 void rr_finished(Process* p) {
-
+  // NO ACTION
 }
