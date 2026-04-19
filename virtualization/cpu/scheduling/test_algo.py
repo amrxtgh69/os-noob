b73a0710401a7087324a76f562bd8a1b0784b7b7
amrxtgh <ghimireamrit619@gmail.com>
2026-04-19 09:59:36 +0545

    added: test_algo

--- virtualization/cpu/scheduling/test_algo.py
+++ virtualization/cpu/scheduling/test_algo.py
@@ -10,3 +10,14 @@ for test in data["tests"]:
         algo = run["algo"]
 
         cmd = ["./simulator", "--algo", algo, "--workload", workload]
+        # dynamically attach params
+        for key, value in run.items():
+            if key == "algo":
+                continue
+
+            if isinstance(value, list):
+                cmd += [f"--{key}"] + list(map(str, value))
+            else:
+                cmd += [f"--{key}", str(value)]
+
+        subprocess.run(cmd)
