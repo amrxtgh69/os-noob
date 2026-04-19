a21cbf1506b00e494bea76aafec8866ae8814778
amrxtgh <ghimireamrit619@gmail.com>
2026-04-19 09:48:53 +0545

    test automation

---
+++ virtualization/cpu/scheduling/test_algo.py
@@ -0,0 +1,12 @@
+import json
+import subprocess
+
+with open("workloads/test.tst") as f:
+    data = json.load(f)
+
+for test in data["tests"]:
+    workload = test[workload]
+    for run in test["runs"]:
+        algo = run["algo"]
+
+        cmd = ["./simulator", "--algo", algo, "--workload", workload]
